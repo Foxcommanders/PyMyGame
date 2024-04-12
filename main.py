9d16e89b3ae63ea601abc2372aac33d2795056e7
Foxcommanders <131612658+Foxcommanders@users.noreply.github.com>
2024-04-12 11:25:30 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 import pygame
+from pygame.constants import QUIT
 
 pygame.init()
 
 HEIGHT = 800
 WIDTH = 1200
 
-main_display = pygame.display.set_mode((HEIGHT, WIDTH))
+main_display = pygame.display.set_mode((WIDTH, HEIGHT))
 
+playing = True
 
-while True:
-    pass
+while playing:
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            playing = False
