1ac903c8957fd1363f2b153f63f6620337f59c4f
Foxcommanders <131612658+Foxcommanders@users.noreply.github.com>
2024-04-12 11:18:30 +0300

    Update main.py

--- main.py
+++ main.py
@@ -7,9 +7,6 @@ WIDTH = 1200
 
 main_display = pygame.display.set_mode((HEIGHT, WIDTH))
 
-count = 0
+
 while True:
-    print("Hello from pygame")
-    count +=1
-    if count >=10000:
-        break
+    pass
