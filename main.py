e398414843ea2931e47d9708901b9eeec2c97f68
Foxcommanders <131612658+Foxcommanders@users.noreply.github.com>
2024-04-12 13:26:30 +0300

    Update main.py

--- main.py
+++ main.py
@@ -36,10 +36,10 @@ while playing:
     if player_rect.right >= WIDTH:
         player_speed[0] = -player_speed[0]
 
-    if player_rect.top <= -1:
+    if player_rect.top < 0:
         player_speed[1] = -player_speed[1]
 
-    if player_rect.left <= -1:
+    if player_rect.left < 0:
         player_speed[0] = -player_speed[0]
  
     main_display.blit(player, player_rect)
