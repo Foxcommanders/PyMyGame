8e120d7c107d477fcef42ed934fe091348a3b637
Foxcommanders <131612658+Foxcommanders@users.noreply.github.com>
2024-04-12 23:05:35 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import random
 
 import pygame
-from pygame.constants import QUIT, K_DOWN, K_RIGHT, K_UP, K_LEFT
+from pygame.constants import QUIT, K_DOWN, K_UP, K_LEFT, K_RIGHT
 
 pygame.init()
 
@@ -21,6 +21,10 @@ player.fill(COLOR_WHITE)
 player_rect = player.get_rect()
 # player_speed = [1, 1]
 player_move_down = [0, 1]
+player_move_right = [1, 0]
+player_move_up = [0, -1]
+player_move_left = [-1, 0]
+
 
 playing = True
 
@@ -35,8 +39,17 @@ while playing:
 
     keys = pygame.key.get_pressed()
 
-    if keys[K_DOWN]:
+    if keys[K_DOWN] and player_rect.bottom < HEIGHT:
         player_rect = player_rect.move(player_move_down)
+
+    if keys[K_RIGHT] and player_rect.right < WIDTH:
+        player_rect = player_rect.move(player_move_right)
+
+    if keys[K_UP] and player_rect.top > 0:
+        player_rect = player_rect.move(player_move_up)
+
+    if keys[K_RIGHT] and player_rect.left > 0:
+        player_rect = player_rect.move(player_move_left)
 
     # if player_rect.bottom >= HEIGHT:
     #     player_speed = random.choice(([1, -1], [-1, -1]))
