0674596f82b5f7488492c6a5d2bf6d1068e3601e
Foxcommanders <131612658+Foxcommanders@users.noreply.github.com>
2024-04-12 13:29:35 +0300

    Update main.py

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ player_size = (20, 20)
 player = pygame.Surface(player_size)
 player.fill(COLOR_WHITE)
 player_rect = player.get_rect()
-player_speed = [1, 1]
+player_speed = [-1, -1]
 
 playing = True
 
@@ -36,10 +36,10 @@ while playing:
     if player_rect.right >= WIDTH:
         player_speed[0] = -player_speed[0]
 
-    if player_rect.top < 0:
+    if player_rect.top <= 0:
         player_speed[1] = -player_speed[1]
 
-    if player_rect.left < 0:
+    if player_rect.left <= 0:
         player_speed[0] = -player_speed[0]
  
     main_display.blit(player, player_rect)
