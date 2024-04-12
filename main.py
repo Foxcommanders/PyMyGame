72e3a8847e7a7ef2ef357f8635c1069d52ab31b8
Foxcommanders <131612658+Foxcommanders@users.noreply.github.com>
2024-04-12 19:41:46 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import random
+
 import pygame
 from pygame.constants import QUIT
 
@@ -22,13 +24,25 @@ player_speed = [-1, -1]
 playing = True
 
 while playing:
-    FPS.tick(120)
+    FPS.tick(660)
 
     for event in pygame.event.get():
         if event.type == QUIT:
             playing = False
 
     main_display.fill(COLOR_BLACK)
+
+    if player_rect.bottom >= HEIGHT:
+        player_speed = random.choice(([1, -1], [-1, -1]))
+
+    if player_rect.top <= 0:
+        player_speed = random.choice(([-1, 1], [1, 1]))
+
+    if player_rect.right >= WIDTH:
+        player_speed = random.choice(([-1, -1], [-1, 1]))
+
+    if player_rect.left <= 0:
+        player_speed = random.choice(([1, 1], [1, -1]))
 
     # if player_rect.bottom >= HEIGHT or player_rect.top <= 0:
     #     player_speed[1] = -player_speed[1]
