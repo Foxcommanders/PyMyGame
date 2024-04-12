82ce2c30527a8fb7a81d3bf9eb97f387ef1c8e69
Foxcommanders <131612658+Foxcommanders@users.noreply.github.com>
2024-04-12 11:33:35 +0300

    Update main.py

--- main.py
+++ main.py
@@ -6,10 +6,13 @@ pygame.init()
 HEIGHT = 800
 WIDTH = 1200
 
+COLOR_WHITE = (255, 255, 255)
+
 main_display = pygame.display.set_mode((WIDTH, HEIGHT))
 
-player = pygame.Surface((20, 20))
-player.fill((255, 255, 255))
+player_size = (20, 20)
+player = pygame.Surface(player_size)
+player.fill(COLOR_WHITE)
 
 playing = True
 
